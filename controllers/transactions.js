f4c94e1e2a90ac0f16755d82464d82058d2099b0
folka2134 <folka2134@gmail.com>
2022-01-06 13:16:47 +0000

    Created controllers file and added 3 primary methods

---
+++ controllers/transactions.js
@@ -0,0 +1,17 @@
+// Get all transactions
+// @route GET /api/v1/transactions
+exports.getTransactions = (req, res, next) => {
+    res.send('GET transactions')
+}
+
+// Add transactions
+// @route POST /api/v1/transactions
+exports.getTransactions = (req, res, next) => {
+    res.send('POST transactions')
+}
+
+// Delete transactions
+// @route DELETE /api/v1/transactions/:id
+exports.getTransactions = (req, res, next) => {
+    res.send('DELETE transactions')
+}
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,8 @@
 const express = require('express')
 
 const router = express.Router()
+const { getTransactions } = require('../controllers/transactions')
 
-router.get("/", (req, res) => {
-    res.send('hello')
-})
+router.route("/").get(getTransactions)
 
 module.exports = router
