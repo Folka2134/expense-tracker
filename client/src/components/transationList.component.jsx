b5112fa37e2b36f14a89dcbac04df3157a837305
folka2134 <folka2134@gmail.com>
2022-01-06 15:52:35 +0000

    Updated addTransaction to update database

--- client/src/components/transationList.component.jsx
+++ client/src/components/transationList.component.jsx
@@ -15,7 +15,7 @@ export const TransationList = () => {
         <>
             <h3>History</h3>
             <ul className="list">
-                {transactions.map((transaction) => (<Transaction key={transaction.id} transaction={transaction}/>))}
+                {transactions.map((transaction) => (<Transaction key={transaction._id} transaction={transaction}/>))}
             </ul>
         </>
     )
