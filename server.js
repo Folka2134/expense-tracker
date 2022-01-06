3097e0ea908d8a686d44c08c5f686932a05647f2
folka2134 <folka2134@gmail.com>
2022-01-06 14:42:31 +0000

    Created mongodb database and connected to it within  db.js and server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const dotenv = require('dotenv')
 const colors = require('colors')
 const morgan = require('morgan')
 
+const connectDB = require('./config/db')
 const transactions = require('./routes/transactions')
-
-const app = express()
-
 dotenv.config({ path: './config/config.env' })
 const PORT = process.env.PORT || 5000
+
+const app = express()
+connectDB()
+
 
 app.use('/api/v1/transactions', transactions)
 
