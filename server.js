f8388f00ff7d572bbe6709807511153262e877a7
folka2134 <folka2134@gmail.com>
2022-01-06 16:03:42 +0000

    Implemented build on client and to run for production

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
@@ -14,9 +15,14 @@ connectDB()
 
 app.use(express.json())
 app.use('/api/v1/transactions', transactions)
+
 if (process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'))
 }
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
 
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on PORT:${PORT}`.yellow.bold));
